b66ec7c5a27bc89f5128572f0f3bb2b67343d317
Al Kamber <al.kamber@mail.polimi.it>
2024-09-17 00:16:03 +0200

    Starting again

---
+++ main.py
@@ -0,0 +1,25 @@
+import pymongo
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# function to get the data from mongoDB
+def fetch_data(client=None):
+    
+    pass
+
+# function to preprocess the data
+def preprocess(X, y):
+    
+    pass
+
+# function to create the model for classifying
+def create_model(input_shape, num_classes):
+    
+    pass
+
+# main execution
+if __name__ == "__main__":
+    
+    pass
